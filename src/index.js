45c865aef8b8f589d99c1e900b30c665ffca0e3a
ihargit <ihar.kharashun@gmail.com>
2019-03-19 11:58:36 +0300

    feat: code added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ module.exports = function getZerosCount(number, base) {
   trailingNulls = 10000000000; // initial amount of trailing zeros for comparison purposes
   for (let i = 2; i <= base; i ++ ) {
     if (base % i === 0) {
-      let numberOfEntries = 0;
+      let entriesInBase = 0;
       let baseReminder = base;
       while (baseReminder % i === 0) {
-        numberOfEntries++;
+        entriesInBase++;
         baseReminder /= i;
       }
-      
+      let numberReminder = number;
+      let entriesInNumber = 0;
+      while (numberReminder / i > 0) {
+        entriesInNumber++;
+        numberReminder /= i;
+      }
+      trailingNulls = Math.min(trailingNulls, entriesInNumber / entriesInBase);
     }
   }
+  return trailingNulls;
 }
