5c0cd294222851763c3eea0eb0cd89abbc1cfb43
ihargit <ihar.kharashun@gmail.com>
2019-03-19 14:40:57 +0300

    fix: fix and extend the code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 module.exports = function getZerosCount(number, base) {
   trailingNulls = 10000000000; // initial amount of trailing zeros for comparison purposes
   for (let i = 2; i <= base; i ++ ) {
-    if (base % i === 0) {
-      let entriesInBase = 0;
+    // select all prime factors of the base that are prime numbers too
+    if (base % i === 0 && isPrime(i)) {
+      let entriesInBase = 0; // counter of entries of the prime factor in base
       let baseReminder = base;
+      // find out number of entries in base for current prime factor 
       while (baseReminder % i === 0) {
         entriesInBase++;
         baseReminder /= i;
       }
+      let entriesInNumber = 0; // counter of entries of the prime factor in the number
       let numberReminder = number;
-      let entriesInNumber = 0;
+      // find out number of entries in the number for current prime factor
       while (numberReminder / i > 0) {
-        entriesInNumber++;
-        numberReminder /= i;
+        entriesInNumber += Math.floor(numberReminder / i);
+        numberReminder = Math.floor(numberReminder / i);
       }
-      trailingNulls = Math.min(trailingNulls, entriesInNumber / entriesInBase);
+      // find out the prime factor with minimal entries in the number
+      trailingNulls = Math.min(trailingNulls, Math.floor(entriesInNumber / entriesInBase));
     }
+    // check if the prime factor is prime number
+    function isPrime(num) {
+      for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+          if(num % i === 0) return false; 
+      return num > 1;
+  }
   }
   return trailingNulls;
 }
