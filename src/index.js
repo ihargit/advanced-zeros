974b6260aee5acc86fd988857eaf6f8e416efea5
ihargit <ihar.kharashun@gmail.com>
2019-03-19 11:17:35 +0300

    feat: add part-ready solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  trailingNulls = 10000000000; // initial amount of trailing zeros for comparison purposes
+  for (let i = 2; i <= base; i ++ ) {
+    if (base % i === 0) {
+      let numberOfEntries = 0;
+      let baseReminder = base;
+      while (baseReminder % i === 0) {
+        numberOfEntries++;
+        baseReminder /= i;
+      }
+      
+    }
+  }
 }
